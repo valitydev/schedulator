eedd39178d4b2af14fe860cec56e2cc8ed7470dc
Inal Arsanukaev <inalarsanukaev@MacBook-Pro-Inal.local>
2019-02-18 16:51:56 +0300

    BJ-376: Init

---
+++ src/main/java/com/rbkmoney/schedulator/SchedulatorApplication.java
@@ -0,0 +1,15 @@
+package com.rbkmoney.schedulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan
+@SpringBootApplication
+public class SchedulatorApplication extends SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulatorApplication.class);
+    }
+
+}
